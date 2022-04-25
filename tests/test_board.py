b4396c6706570d3380e8e593a4e93ffe755167a3
a5chin <81167661+a5chin@users.noreply.github.com>
2022-04-26 00:29:33 +0900

    update badges in README.md (#1)
    * update badge in README.md
    
    * fixed linting in tests

--- tests/test_board.py
+++ tests/test_board.py
@@ -1,26 +1,27 @@
 import sys
-import pathlib
+from pathlib import Path
+
 import numpy as np
+import pytest
 from torch.utils.tensorboard import SummaryWriter
 
-current_dir = pathlib.Path(__file__).resolve().parent
-sys.path.append( str(current_dir) + '/../' )
+current_dir = Path(__file__).resolve().parent
+sys.path.append(str(current_dir) + "/../")
 
 from board import Board
 
 
-NUM = 100
-LOG_DIR = "./logs"
-OUT_DIR = "./outputs"
+@pytest.mark.parametrize(
+    ("log_dir", "output_dir", "freq"), [("./logs", "./outputs", 100)]
+)
+def test_board(log_dir, output_dir, freq):
+    random = np.random.randn(freq)
+    writer = SummaryWriter(log_dir=log_dir)
+    board = Board(log_dir=log_dir)
 
-def test_board():
-    random = np.random.randn(NUM)
-    writer = SummaryWriter(log_dir=LOG_DIR)
-    board = Board(log_dir=LOG_DIR)
-
-    for i in range(NUM):
+    for i in range(freq):
         writer.add_scalar("random", random[i], i)
 
-    board.savefig(output_dir=OUT_DIR)
+    board.savefig(output_dir=output_dir)
 
     assert True
